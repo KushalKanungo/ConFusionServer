b08a15c081d6597f0fface304fbb535cd9101cc2
Kushal <kushalkhandelwal27@gmail.com>
2022-01-30 00:35:36 +0530

    Passport Local

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,7 +6,8 @@ const logger = require('morgan');
 const mongoose = require('mongoose')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 const Dishes = require('./models/dishes')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -40,26 +41,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+var passport = require('passport');
 router.use(express.json())
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+/*
 router.post('/signup', (req, res, next) => {
   User.findOne({ username: req.body.username })
     .then((user) => {
@@ -73,6 +75,31 @@ router.post('/login', (req, res, next) => {
     res.end('You are already authenticated!');
   }
 })
+*/
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
+      }
+    });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, status: 'You are successfully logged in!' });
+});
 
 router.get('/logout', (req, res) => {
   if (req.session) {
